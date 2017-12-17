a547a9a3412213deec8c040ef08744d9b76d1712
David Samuel <cryptondave@gmail.com>
2017-12-16 21:56:44 -0800

    renders empt array from state

--- js/index.js
+++ js/index.js
@@ -1,2 +1,12 @@
 require('babel-polyfill');
 
+// js/components/index.js
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Board = require('./components/board');
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    ReactDOM.render(<Board title="Dave's Board" />, document.getElementById('app'));
+});
